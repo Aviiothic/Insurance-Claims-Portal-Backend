52fbf7a998ce2bb81a2e0255ce9f0e24f8796761
Vaishnavi333-coder <151718327+Vaishnavi333-coder@users.noreply.github.com>
2025-11-28 15:29:57 +0530

    Update create-claim.dto.ts

--- src/claims/dto/create-claim.dto.ts
+++ src/claims/dto/create-claim.dto.ts
@@ -1,1 +1,14 @@
-export class CreateClaimDto {}
+import { IsNumber, IsString, IsNotEmpty, MaxLength } from "class-validator"
+
+export class CreateClaimDto {
+  @IsNumber()
+  policyId: number;
+
+  @IsNumber()
+  claimAmt: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(200)
+  description: string;
+}
