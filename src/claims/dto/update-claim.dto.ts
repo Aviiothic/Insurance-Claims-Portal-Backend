d1328138104e07ea13b9d7a09ed39f6c43a018f5
Vaishnavi333-coder <151718327+Vaishnavi333-coder@users.noreply.github.com>
2025-11-28 15:30:27 +0530

    Update update-claim.dto.ts

--- src/claims/dto/update-claim.dto.ts
+++ src/claims/dto/update-claim.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClaimDto } from './create-claim.dto';
 
-export class UpdateClaimDto extends PartialType(CreateClaimDto) {}
+import { IsString, IsOptional, MaxLength, IsNumber } from 'class-validator';
+
+export class UpdateClaimDto {
+  @IsOptional()
+  @IsNumber()
+  claimAmt?: number;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(200)
+  description?: string;
+}
